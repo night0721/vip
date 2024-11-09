546a6c5f1a5159b06ffb5e9dddc67129c4bd59be
night0721 <night@night0721.xyz>
2024-11-09 00:40:00 +0000

    Remove old code

--- include/bar.h
+++
@@ -1,8 +0,0 @@
-#ifndef BAR_H_
-#define BAR_H_
-
-void draw_status_bar(struct abuf *ab);
-void draw_message_bar(struct abuf *ab);
-void set_status_bar_message(const char *fmt, ...);
-
-#endif
--- include/io.h
+++
@@ -1,9 +0,0 @@
-#ifndef IO_H_
-#define IO_H_
-
-int read_key(void);
-void save_file(void);
-void process_key(void);
-void draw_rows(struct abuf *ab);
-
-#endif
